a4b1b4707ab174fed72ad6fd7ff4a622c4f8d30d
Holly <hollyduckworth@hotmail.co.uk>
2020-03-26 22:21:34 +0000

    0-10 questions completed for now

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,19 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(catalogueService.checkBookByTitle("The Assassination of Margaret Thatcher")).toBe(true);
+      expect(catalogueService.checkBookByTitle("Adrian Mole")).toBe(false);
+    });
+  });
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns no. book starting with that letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("T")).toBe(5);
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+      expect(catalogueService.countBooksByFirstLetter("w")).toBe(2);
+    });
+  });
 });
